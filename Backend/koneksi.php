faad8a653d92288a6d55b4ace7658e413dc447ca
reinnlerr <birelramadhan@gmail.com>
2026-05-18 18:29:59 +0700

    koneksi.php

--- Backend/koneksi.php
+++ Backend/koneksi.php
@@ -8,10 +8,9 @@ $db   = "db_konveksi"; // Pastikan ini sama dengan nama database kamu
 // Membuat Koneksi
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
-// Cek Koneksi (Untuk bukti laporan)
+// Cek Koneksi
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
-} else {
-    echo "Koneksi ke Database " . $db . " Berhasil!";
 }
+// Bagian "else echo" dihapus total biar respon JSON ke React bersih 100%
 ?>
