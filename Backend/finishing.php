cf52be06cd682b069c840919f822f8f8fc2e4b71
reinnlerr <birelramadhan@gmail.com>
2026-05-24 11:04:26 +0700

    update

--- Backend/finishing.php
+++ Backend/finishing.php
@@ -1,11 +1,15 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 include "koneksi.php";
-
 $method = $_SERVER['REQUEST_METHOD'];
 
 switch ($method) {
